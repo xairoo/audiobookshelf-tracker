311e9d0988b5b25e156f0244c4148e9de6a8a1da
Xairoo <731894+xairoo@users.noreply.github.com>
2024-12-30 00:31:31 +0100

    user switch added, style improved

--- client/pages/user/[userId].jsx
+++ client/pages/user/[userId].jsx
@@ -4,50 +4,121 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import fetcher from "../../utils/fetcher";
 import Protected from "../../components/protected";
-import styles from "./../login.module.css";
 import styled from "@emotion/styled";
 
-const ProgressWrapper = styled.div`
-  width: 150px;
-  height: 1.5rem;
-  line-height: 1.5rem;
-  margin: 0.5rem;
-  color: #fff;
-  background: #b366f9;
-  border: none;
-  border-radius: 3px;
-  cursor: pointer;
-  text-align: right;
+const UserWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Select = styled.select`
+  appearance: none;
+  width: 200px;
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
+  background-color: #380568;
+  border: 2px solid #380568;
+  border-radius: 0.5rem;
+  box-shadow: none;
+  color: #fff;
+  cursor: pointer;
+  :focus {
+    outline: none;
+    outline-offset: 0;
+    border: 2px solid #380568;
+  }
+`;
+
+const SearchWrapper = styled.div`
   position: relative;
-  /* justify-content: right; */
+  align-items: center;
+  display: flex;
+  margin: 1rem 0;
+`;
+
+const Search = styled.input`
+  height: 20px;
+  width: 100%;
+  background: transparent;
+  border: 2px solid #37095f;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
+  box-shadow: none;
+  color: #fff;
+  ::placeholder {
+    color: #8a8a8a;
+  }
+  :focus {
+    outline: none;
+    outline-offset: 0;
+    border: 2px solid #7627c0;
+  }
+`;
+
+const SearchIconWrapper = styled.div`
+  background: #380568;
+  position: absolute;
+  right: 0.25rem;
+  top: 0.25rem;
+  bottom: 0.25rem;
+  display: flex;
+  height: 1.5rem;
+  width: 1.5rem;
+  border-radius: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SearchIcon = styled.svg`
+  height: 1rem;
+  width: 1rem;
+`;
+
+const Entry = styled.div`
+  background: #23063d;
+  margin: 0.75rem 0;
+  padding: 0.25rem 0.5rem;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-radius: 0.5rem;
+  overflow: hidden;
+`;
+
+const Meta = styled.div`
+  margin: 0.25rem 0.5rem;
+  right: 0.25rem;
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+const Title = styled.div`
+  z-index: 5;
+`;
+
+const ProgressValue = styled.div`
+  z-index: 5;
 `;
 
 const ProgressBar = styled.div`
   position: absolute;
-  left: 0;
   top: 0;
+  left: 0;
+  right: 0;
   bottom: 0;
+  z-index: 1;
   width: ${(props) => (props.progress ? props.progress : `0`)}%;
   height: 100%;
   background: #7627c0;
+  border-radius: 0.5rem;
 `;
-
-const ProgressValue = styled.div`
-  position: absolute;
-  right: 0.25rem;
-  top: 0;
-  bottom: 0;
-`;
-
-const Progress = ({ progress }) => {
-  return (
-    <ProgressWrapper>
-      <ProgressBar progress={progress} />
-      <ProgressValue>{progress}%</ProgressValue>
-    </ProgressWrapper>
-  );
-};
 
 export default function Home() {
   const { status, data: session, token } = useSession();
@@ -60,6 +131,20 @@ export default function Home() {
     setSearch(event.target.value);
     setSearchStr(event.target.value.toLowerCase());
   };
+
+  const { data: users, error: errorUsers } = useSWR(
+    token && status === "authenticated" && userId
+      ? {
+          url: `/api/v1/users`,
+          method: "GET",
+          token,
+        }
+      : null, // Fetch only if bearer token is set
+    fetcher,
+    {
+      revalidateOnFocus: false,
+    }
+  );
 
   const { data: user, error: errorUser } = useSWR(
     token && status === "authenticated" && userId
@@ -116,53 +201,73 @@ export default function Home() {
       <>
         <h1>History</h1>
 
-        <div>
-          <ul>User: {!errorUser && user && user.username}</ul>
-        </div>
+        <UserWrapper>
+          <div>User:</div>
+          <Select
+            value={user?.id}
+            onChange={(e) => router.push(`/user/${e.target.value}`)}
+          >
+            {users?.map((user) => {
+              return (
+                <option key={user.id} value={user.id}>
+                  {user.username}
+                </option>
+              );
+            })}
+          </Select>
+        </UserWrapper>
 
+        <SearchWrapper>
+          <Search
+            onChange={handleSearch}
+            value={search}
+            placeholder="Author, Title, ..."
+          />
+          <SearchIconWrapper>
+            <SearchIcon
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" />
+            </SearchIcon>
+          </SearchIconWrapper>
+        </SearchWrapper>
         <div>
-          <label className={styles.label}>
-            <div className={styles.name}>Search:</div>
-            <input
-              className={styles.input}
-              type="text"
-              name="username"
-              onChange={handleSearch}
-              value={search}
-            />
-          </label>
-        </div>
-
-        <div>
-          <ul>
-            {!errorHistory &&
-              history &&
-              history
-                .filter((item) => {
-                  if (searchStr) {
-                    return (
-                      item.author?.toLowerCase().includes(searchStr) ||
-                      item.podcastTitle?.toLowerCase().includes(searchStr) ||
-                      item.title?.toLowerCase().includes(searchStr) ||
-                      item.subtitle?.toLowerCase().includes(searchStr)
-                    );
-                  } else {
-                    return item.progress > 0;
-                  }
-                  // return item.progress === 1;
-                })
-                .map((item) => {
+          {!errorHistory &&
+            history &&
+            history
+              .filter((item) => {
+                if (searchStr) {
                   return (
-                    <li key={`${item.itemId}-${item.episodeId}`}>
-                      {item.author || item.podcastTitle} - {item.title}{" "}
-                      {item.subtitle && `- ${item.subtitle}`}
-                      <Progress
-                        progress={parseFloat((item.progress * 100).toFixed(2))}
-                      />
-                    </li>
+                    item.author?.toLowerCase().includes(searchStr) ||
+                    item.podcastTitle?.toLowerCase().includes(searchStr) ||
+                    item.title?.toLowerCase().includes(searchStr) ||
+                    item.subtitle?.toLowerCase().includes(searchStr)
                   );
-                })}
-          </ul>
+                } else {
+                  return item.progress > 0;
+                }
+                // return item.progress === 1;
+              })
+              .map((item) => {
+                return (
+                  <Entry key={`${item.itemId}-${item.episodeId}`}>
+                    <Meta>
+                      <Title>
+                        {item.author || item.podcastTitle} - {item.title}{" "}
+                        {item.subtitle && `- ${item.subtitle}`}
+                      </Title>
+                      <ProgressValue>
+                        {parseFloat((item.progress * 100).toFixed(0))}%
+                      </ProgressValue>
+                    </Meta>
+                    <ProgressBar
+                      progress={parseFloat((item.progress * 100).toFixed(2))}
+                    />
+                  </Entry>
+                );
+              })}
         </div>
       </>
     );
